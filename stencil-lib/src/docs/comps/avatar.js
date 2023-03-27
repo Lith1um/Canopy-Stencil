44e7ee26b551a7598813f50d9ed80bcbd41206a6
Alex Rayner <alexrayner94@gmail.com>
2023-03-27 19:16:56 +0100

    update avatar docs

--- stencil-lib/src/docs/comps/avatar.js
+++ stencil-lib/src/docs/comps/avatar.js
@@ -1,13 +1,9 @@
-(function () {
-  document.getElementById('avatar-html-example').code = `<cpy-avatar src="https://react.semantic-Ui.com/images/avatar/large/matthew.png"></cpy-avatar>
+(function () {  
+  document.getElementById('comp-styles').tableData = [
+    { cssVariable: '--cpy-avatar-bg', description: 'Background colour for avatar', defaultValue: '--cpy-bg-medium' },
+  ];
 
-<cpy-avatar
-  type="square"
-  size="large"
-  src="https://react.semantic-Ui.com/images/avatar/large/matthew.png">
-</cpy-avatar>`;
-
-  document.getElementById('avatar-props').tableData = [
+  document.getElementById('comp-props').tableData = [
     { name: 'border', description: 'Shows a border around the avatar', type: 'boolean', default: "false", required: false },
     { name: 'size', description: 'Size of the avatar', type: "'default' | 'large' | 'small' | 'x-large'", default: "'default'", required: false },
     { name: 'src', description: 'Image source for the avatar', type: 'string', default: "", required: false },
